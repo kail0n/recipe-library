86344456bdf82617ad597529b0dbc05894e32f73
kail0n <kailontse@gmail.com>
2020-12-31 14:55:46 +0000

    db config and seed

---
+++ server-postgresql/db/config.js
@@ -0,0 +1,8 @@
+const { Pool } = require('pg');
+const pool = new Pool({database: 'cookbook'});
+
+const run = (q, values, callback) => {
+    return pool.query(q, values, callback);
+};
+
+module.exports= ( run )
---
+++ server-postgresql/db/seeds.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS recipes;
+
+CREATE TABLE recipes (
+    id serial PRIMARY KEY,
+    name varchar(255) NOT NULL,
+    author varchar(255),
+    serves int NOT NULL,
+);
+
+INSERT INTO recipes (name, author, serves)
+VALUES
+    ('Mac n cheese', 'Kai', 4)
+    ('Spaghetti bolognese', 'Kayi', 4)
+    ('Burrito', 'Kayi', 2)
+    
